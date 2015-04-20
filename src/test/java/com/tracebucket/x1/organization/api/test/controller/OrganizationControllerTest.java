a622f646c3adefdf5feee81ca910070a63773107
sadath <syed.sadath@mmpsd.com>
2015-04-20 15:09:10 +0530

    OrganizationController Test Case changed to use RestTemplate

--- src/test/java/com/tracebucket/x1/organization/api/test/controller/OrganizationControllerTest.java
+++ src/test/java/com/tracebucket/x1/organization/api/test/controller/OrganizationControllerTest.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tracebucket.x1.organization.api.DefaultOrganizationStarter;
 import com.tracebucket.x1.organization.api.rest.resource.DefaultOrganizationResource;
 import com.tracebucket.x1.organization.api.rest.resource.DefaultOrganizationUnitResource;
-import com.tracebucket.x1.organization.api.service.DefaultOrganizationService;
 import com.tracebucket.x1.organization.api.test.fixture.DefaultOrganizationResourceFixture;
 import com.tracebucket.x1.organization.api.test.fixture.DefaultOrganizationUnitResourceFixture;
 import org.junit.After;
@@ -15,63 +14,40 @@ import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.test.ConfigFileApplicationContextInitializer;
-import org.springframework.boot.test.IntegrationTest;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
-import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockServletContext;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.boot.test.WebIntegrationTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.web.context.WebApplicationContext;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.asyncDispatch;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.request;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+import org.springframework.web.client.RestTemplate;
 
 /**
- * Created by sadath on 10-Feb-15.
+ * Created by sadath on 20-Apr-15.
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@WebAppConfiguration
-@SpringApplicationConfiguration(classes  = {DefaultOrganizationStarter.class})
+@SpringApplicationConfiguration(classes = DefaultOrganizationStarter.class)
+@WebIntegrationTest
 public class OrganizationControllerTest {
 
     private static final Logger log = LoggerFactory.getLogger(OrganizationControllerTest.class);
 
-    private static MockMvc mockMvc;
-
-    @Autowired
-    private WebApplicationContext wac;
+    RestTemplate restTemplate = null;
 
     @Autowired
     private ObjectMapper objectMapper;
 
+    @Value("http://localhost:${server.port}${server.contextPath}")
+    private String basePath;
+
     private DefaultOrganizationResource organization = null;
 
     @Before
-    public void setUp(){
-        mockMvc = webAppContextSetup(this.wac).build();
+    public void setUp() {
+        restTemplate = new RestTemplate();
     }
 
     private void createOrganization() throws Exception{
         organization = DefaultOrganizationResourceFixture.standardOrganization();
-        MvcResult mvcResult = null;
-        log.info("Add Organization : "+ objectMapper.writeValueAsString(organization));
-        mvcResult = mockMvc
-                .perform(MockMvcRequestBuilders.post("/organization")
-                        .content(objectMapper.writeValueAsString(organization))
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andReturn();
-        organization = objectMapper.readValue(mvcResult.getResponse().getContentAsString(), DefaultOrganizationResource.class);
+        organization = restTemplate.postForObject(basePath+"/organization", organization, DefaultOrganizationResource.class);
         Assert.assertNotNull(organization);
     }
 
@@ -82,54 +58,30 @@ public class OrganizationControllerTest {
     }
 
     @Test
+    public void testFindOne() throws Exception {
+        createOrganization();
+        String uid = organization.getUid();
+        organization = restTemplate.getForObject(basePath + "/organization/" + uid, DefaultOrganizationResource.class);
+        Assert.assertNotNull(organization.getUid());
+        Assert.assertEquals(uid, organization.getUid());
+    }
+
+    @Test
     public void testAddOrganizationUnit() throws Exception{
         createOrganization();
         DefaultOrganizationUnitResource organizationUnit = DefaultOrganizationUnitResourceFixture.standardOrganizationUnitResource();
-        MvcResult mvcResult = null;
-        log.info("Add Organization Unit : "+ objectMapper.writeValueAsString(organizationUnit));
-        mvcResult = mockMvc
-                .perform(MockMvcRequestBuilders.put("/organization/"+organization.getUid()+"/organizationunit/")
-                        .content(objectMapper.writeValueAsString(organizationUnit))
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andReturn();
-        organization = objectMapper.readValue(mvcResult.getResponse().getContentAsString(), DefaultOrganizationResource.class);
-        Assert.assertNotNull(organization);
+        restTemplate.put(basePath+"/organization/"+organization.getUid()+"/organizationunit", organizationUnit);
+        organization = restTemplate.getForObject(basePath + "/organization/" + organization.getUid(), DefaultOrganizationResource.class);
         Assert.assertNotNull(organization.getUid());
         Assert.assertEquals(1, organization.getOrganizationUnits().size());
-    }
-
-
-    @Test
-    public void testFindOne() throws Exception {
-        createOrganization();
-        MvcResult mvcResult = null;
-        log.info("Find Organization By UID : "+ organization.getUid());
-        mvcResult = mockMvc
-                .perform(MockMvcRequestBuilders.get("/organization/"+organization.getUid())
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andReturn();
-        String uid = organization.getUid();
-        organization = objectMapper.readValue(mvcResult.getResponse().getContentAsString(), DefaultOrganizationResource.class);
-        Assert.assertNotNull(organization);
-        Assert.assertNotNull(organization.getUid());
-        Assert.assertEquals(uid, organization.getUid());
     }
 
     @After
     public void tearDown() throws Exception{
         if(organization != null && organization.getUid() != null) {
-            MvcResult mvcResult = null;
-            log.info("Delete Organization By UID : "+ organization.getUid());
-            mvcResult = mockMvc
-                    .perform(MockMvcRequestBuilders.delete("/organization/"+organization.getUid())
-                            .contentType(MediaType.APPLICATION_JSON))
-                    .andExpect(MockMvcResultMatchers.status().isOk())
-                    .andReturn();
-            Boolean status = objectMapper.readValue(mvcResult.getResponse().getContentAsString(), Boolean.class);
-            Assert.assertTrue(status);
+            restTemplate.delete(basePath + "/organization/" + organization.getUid());
+            organization = restTemplate.getForObject(basePath + "/organization/" + organization.getUid(), DefaultOrganizationResource.class);
+            Assert.assertNull(organization.getUid());
         }
     }
-
 }
