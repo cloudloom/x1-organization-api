e60db9ded1bfe7a28a2f2b8f5b9866c87f3aa3fa
sadath <ssmdrepo@gmail.com>
2015-06-15 16:39:25 +0530

    Added ControllerAdviceConfiguration Changed tron-ddd-cqrs to version 0.7

--- src/test/java/com/tracebucket/x1/organization/api/test/controller/OrganizationControllerTest.java
+++ src/test/java/com/tracebucket/x1/organization/api/test/controller/OrganizationControllerTest.java
@@ -2,7 +2,6 @@ package com.tracebucket.x1.organization.api.test.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tracebucket.x1.organization.api.DefaultOrganizationStarter;
-import com.tracebucket.x1.organization.api.domain.impl.jpa.DefaultOrganizationUnit;
 import com.tracebucket.x1.organization.api.rest.resource.*;
 import com.tracebucket.x1.organization.api.test.fixture.*;
 import org.junit.After;
